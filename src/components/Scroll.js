8be9d81d8c35e5b655ef12f3411d6a78e10c9159
Almog <almogbh.l@gmail.com>
2019-01-29 19:17:16 +0200

    first commit

---
+++ src/components/Scroll.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Scroll = (props) => {
+    return (
+        <div style={ {overflowY: 'scroll', border: '5px solid white', height: '800px'} } >
+            {props.children}
+        </div>
+    )
+    
+}
+
+
+
+
+export default Scroll;
